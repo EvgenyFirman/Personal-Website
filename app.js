1080cd40b6ac2f0597c2f463170b4b41240dd0c9
Eugene <Eugeniolio@yandex.com>
2020-03-30 22:44:24 +0300

    1.1.0 version

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const path = require("path")
+const nodemailer = require("nodemailer")
 const bodyParser = require ('body-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("*", function(req,res){
     res.sendFile(path.join(__dirname,"index.html"))
 })
+app.post("/",function(req,res){
+
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'EugeneFirman@gmail.com',
+          pass: 'e121197ft868eb'
+        }
+      });
+  
+      let mailOptions = {
+        from: req.body.email,
+        to: "EugeneFirman@gmail.com", 
+        subject: "From website" ,
+        text: req.body.fname + " " + req.body.email + " " + req.body.ftext 
+        };
+        console.log(req.body.ftext)
+    
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+    });
+    
+
 app.listen(process.env.PORT || 3000)
