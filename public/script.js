0aecd078b5a7f8abcb9fe81be8682fc7ead0d4ea
Eugene <Eugeniolio@yandex.com>
2020-04-19 03:04:32 +0300

    scroll fix

---
+++ public/script.js
@@ -0,0 +1,9 @@
+
+    $(".js--scroll-to-features").click(function(){
+        $("html,body").animate( {scrollTop: $(".js--features").offset().top}, 1000);
+    });
+
+    $(".js--scroll-to-partnership").click(function(){
+        $("html,body").animate({scrollTop: $(".js--partnership").offset().top},1000)
+    })
+
