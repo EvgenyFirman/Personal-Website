ca09af6e2c2c567233413e944676daa0a3622a00
Eugene <Eugeniolio@yandex.com>
2020-04-20 00:46:50 +0300

    1.1.9

--- public/script.js
+++ public/script.js
@@ -1,9 +1,15 @@
+   $(document).ready(function(){
 
     $(".js--scroll-to-features").click(function(){
         $("html,body").animate( {scrollTop: $(".js--features").offset().top}, 1000);
     });
-
     $(".js--scroll-to-partnership").click(function(){
         $("html,body").animate({scrollTop: $(".js--partnership").offset().top},1000)
-    })
+    });
+    $(".js--animated").waypoint(function(direction){
+        $(".js--animated").addClass("animated fadeIn")
+    },{
+        offset: "50%"
+    });
+});
 
